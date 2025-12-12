6869e0a7b015147b10a184d608fc64cd7b87a84d
JaanhviShukla <jaanhvishukla11@gmail.com>
2025-12-12 21:48:42 +0530

    Initial commit

---
+++ src/pages/MyProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyProfile = () => {
+	return (
+		<div>
+			<h1>My Profile</h1>
+			<p>User profile information will appear here.</p>
+		</div>
+	)
+}
+
+export default MyProfile
+
